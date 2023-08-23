6b3d519a6cbc8a1dbffb3fb741ed51bb403f8abe
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-08-23 13:19:17 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import { fetchBreeds, getCatByID } from './js/cat-api.js';
+import SlimSelect from 'slim-select';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  catSelect: document.querySelector('#single'),
+  loader: document.querySelector('.loader-container'),
+  error: document.querySelector('.error'),
+  catInfo: document.querySelector('.cat-info'),
+};
+
+refs.loader.classList.remove('is-hidden');
+refs.catSelect.classList.add('is-hidden');
+
+refs.catSelect.addEventListener('change', onCatSelect);
+
+function onCatSelect(evt) {
+  evt.preventDefault();
+
+  refs.catInfo.innerHTML = '';
+
+  const selectedBreedID = evt.target.value;
+
+  getCatByID(selectedBreedID)
+    .then(data => {
+      refs.loader.classList.remove('is-hidden');
+      setTimeout(() => {
+        renderSelectedCat(
+          data.url,
+          data.breeds[0].name,
+          data.breeds[0].description,
+          data.breeds[0].temperament
+        );
+        refs.loader.classList.add('is-hidden');
+      }, 500);
+    })
+    .catch(err =>
+      Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    );
+}
+
+fetchBreeds()
+  .then(results => {
+    refs.loader.classList.remove('is-hidden');
+
+    const markupForm = results
+      .map(result => renderForm(result.reference_image_id, result.name))
+      .join('');
+
+    refs.catSelect.innerHTML = markupForm;
+
+    new SlimSelect({
+      select: '#single',
+    });
+
+    refs.loader.classList.add('is-hidden');
+    refs.catSelect.classList.remove('is-hidden');
+  })
+  .catch(err =>
+    Notify.failure('Oops! Something went wrong! Try reloading the page!')
+  );
+
+function renderForm(id, name) {
+  return (breedMarkup = `<option value="${id}">${name}</option>`);
+}
+
+function renderSelectedCat(url, name, description, temperament) {
+  const selectedCatMarkup = `<img class="cat-img" src="${url}" alt="${name}" width=400px/>
+  <dl class="cat-txt">
+  <h2 class="cat-name">${name}</h2>
+  <dt class="cat-desc">Description:</dt>
+  <dd>${description}</dd>
+  <dt class="cat-temper">Temperament: </dt>
+  <dd>${temperament}</dd></dl>`;
+  refs.catInfo.insertAdjacentHTML('afterbegin', selectedCatMarkup);
+}
---
+++ src/js/cat-api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const CAT_API_URL = 'https://api.thecatapi.com/v1/';
+const CAT_API_ENDPOINT = 'breeds/';
+const CAT_INFO_URL = 'https://api.thecatapi.com/v1/images/';
+
+const AXIOS_KEY = (axios.defaults.headers.common['x-api-key'] =
+  'api_key=live_1CvzBHR9J6nLSt2wdKpcvsfMKA4iUHP77ZaTYHTAZIqJJMJaOeN1FB0e3mnQgzkP');
+
+export function fetchBreeds() {
+  return fetch(`${CAT_API_URL}${CAT_API_ENDPOINT}`).then(result => {
+    if (result.ok) {
+      return result.json();
+    } else {
+      throw new Error('Error');
+    }
+  });
+}
+
+export function getCatByID(id) {
+  const options = {
+    headers: {
+      'x-api-key': AXIOS_KEY,
+    },
+  };
+
+  const searchUrl = `${CAT_INFO_URL}${id}`;
+
+  return fetch(searchUrl, options)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error();
+      } else {
+        return res.json();
+      }
+    })
+    .catch(err =>
+      Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    );
+}
